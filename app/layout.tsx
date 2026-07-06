2b3e636c72ba3647cd7991aa7b52a6a46b22ddc0
itspriyo-xyz <itspriyo.xyz@gmail.com>
2026-07-06 11:49:35 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Minnat TV",
-  description: "Live TV ",
+  title: "PRIYO TV",
+  description: "Live TV",
 };
 
 export default function RootLayout({
